29104d27f186d6809293236a15fed540acb46b4e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-16 20:27:25 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,8 @@
 
 package { 'flask':
       require  => Package['flask'],
-      ensure   => 'installed'
+      name     => 'flask',
+      ensure   => present
       provider => 'pip3',
       version  => '2.1.0'
 }
