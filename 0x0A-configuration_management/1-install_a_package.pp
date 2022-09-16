45106bac8a51b9475b034e34f7c6187ce076510e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-16 20:23:04 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,8 @@
 # Using Puppet, install flask from pip3.
-include pip
 
-pip::install { 'flask':
-  package        => 'flask',
-  version        => '2.1.0',
-  ensure         => present,
+package { 'flask':
+      require  => Package['flask'],
+      ensure   => 'installed'
+      provider => 'pip3',
+      version  => '2.1.0'
 }
-
-#package { 'pip':
-#require  => Package['python-pip'],
-#ensure   => 'installed'
-#provider => 'pip',
-#name     => 'flask',
-#version  => '2.1.0'}
