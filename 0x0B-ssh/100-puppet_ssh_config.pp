de6f0af62135277a0a0c789e60f968c4913b8bf2
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-19 02:01:15 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 include stdlib
 file_line { 'Declare identity file':
-  path    => '~/.ssh/config',
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '~/.ssh/config',
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
 }
