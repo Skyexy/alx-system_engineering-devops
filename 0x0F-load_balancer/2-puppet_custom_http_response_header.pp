637d3900a3b2d980c8f22a1e9e374165ea8adc9e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-27 23:10:48 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,26 @@
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
-}
+# Setup nginx server
 
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+  ensure     => 'installed',
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file_line { 'b':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
+  line    => 'add_header X-Served-By $HOSTNAME;',
   require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
 service { 'nginx':
