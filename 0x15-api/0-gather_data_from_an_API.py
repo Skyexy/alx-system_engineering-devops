4336b7577c0ae12e8b367332be20f56a1166bea3
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-17 23:09:43 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import requests
+import sys
+
 def main():
-    api_url = 'https://jsonplaceholder.typicode.com/todos/1'
+    api_url = "https://jsonplaceholder.typicode.com/todos/{}".format(sys.argv[i])
     response = requests.get(api_url)
+    print("Employee {} is done with tasks({}/{})\n")
+    print(response.json())
 if __name__ == "__main__":
-    
+    main()
