50c35d00b49a629a08f22c66805249ce45bc679d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-17 23:43:05 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,7 +13,7 @@ def main():
     task_done = 0
     
     for i in  r.json():
-        if i.get('completed') == "True":
+        if i.get('completed') == True:
             task_done += 1
     print("\nEmployee {} is done with tasks({}/{})\n")
     print(r.json())
