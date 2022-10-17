ec04e4681c04a5d5e5088a317dab128a40f804da
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-17 23:53:52 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,21 @@ import requests
 import sys
 
 def main():
-    api_url = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
-    response = requests.get(api_url)
-    name = response.json().get('name')
-    
-    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                     format(sys.argv[1]))
-    total_numof_task = len(r.json())
-    task_done = 0
-    title = ""
+	api_url = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+	response = requests.get(api_url)
+	name = response.json().get('name')
+	r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+					 format(sys.argv[1]))
+	total_numof_task = len(r.json())
+	task_done = 0
+	title = ""
 
-    for i in  r.json():
-        if i.get('completed') == True:
-            task_done += 1
-            title += "\t {}\n".format(i.get('title'))
+	for i in  r.json():
+		if i.get('completed') == True:
+			task_done += 1
+			title += "\n\t {}".format(i.get('title'))
+			
+	print("\nEmployee {} is done with tasks({}/{}):{}".format(name, task_done, total_numof_task, title))
 
-    
-    print("\nEmployee {} is done with tasks({}/{})\n{}".format(name, task_done, total_numof_task, title))
 if __name__ == "__main__":
-    main()
+	main()
