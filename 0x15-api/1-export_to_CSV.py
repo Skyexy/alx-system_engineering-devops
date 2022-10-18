94631bdf988a9edb5ad5cfef06df0c94e14a32f7
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-18 01:14:51 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-For a given employee ID, return information about TODO list progress
+For a given employee ID, export to CSV information about TODO list progress
 """
 
 if __name__ == '__main__':
     import requests
-    from sys import argv
     import csv
+    from sys import argv
 
     r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
                      format(argv[1]))
-    name = r.json().get('username')
-    
-    res = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+    username = r.json().get('username')
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
                      format(argv[1]))
-    long = []
-    
-    for lop in res.json():
-        title = lop.get('title')
-        subject = "{}".format(argv[1]),"{}".format(name),"{}".format(lop.get('completed')),"{}".format(title)
-        with open("{}.csv".format(argv[1]), 'a') as file:
-            writer = csv.writer(file)
-            writer.writerow(subject)
+    data = r.json()
+
+    with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in data:
+            csv_writer.writerow([argv[1], username, task.get('completed'),
+                                 task.get('title')])
