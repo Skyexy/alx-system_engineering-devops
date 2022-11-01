cd4560fb79424561c1bf1ee78f9593c256e380f3
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-11-01 17:24:13 +0100

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles
+"""
+
+def def top_ten(subreddit):
+    try:
+        base_ulr = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        res = requests.get(base_ulr, headers = {'User-agent': 'yourbot'})
+        for x in range(10):
+            print (res.json()['data']['children'][x]['data']['title'])
+    except:
+        print (None)
