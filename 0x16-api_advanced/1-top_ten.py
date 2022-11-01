651f4830da32779ced950e2a991d8c860af9a921
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-11-01 19:37:27 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ queries the Reddit API and prints the titles
 """
 import requests
 
-def def top_ten(subreddit):
+def top_ten(subreddit):
     try:
         base_ulr = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
         res = requests.get(base_ulr, headers = {'User-agent': 'custom'}, allow_redirects=False)
