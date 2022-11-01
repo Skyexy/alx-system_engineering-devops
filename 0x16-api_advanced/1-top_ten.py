c3d596547c7ce8cddff40f94d8f6e22c91f7d6e9
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-11-01 18:54:36 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ queries the Reddit API and prints the titles
 def def top_ten(subreddit):
     try:
         base_ulr = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        res = requests.get(base_ulr, headers = {'User-agent': 'yourbot'})
+        res = requests.get(base_ulr, headers = {'User-agent': 'custom'})
         for x in range(10):
             print (res.json()['data']['children'][x]['data']['title'])
     except:
